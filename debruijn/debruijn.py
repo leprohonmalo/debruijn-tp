824621c39ae9213df9a67c9d53a435deeac58228
leprohonmalo <leprohonmalo@gmail.com>
2019-10-22 15:35:09 +0200

    Added function for building of graph from kmer, Added functions for getting starting and ending nodes of a graph

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -11,7 +11,7 @@ import sys
 import getopt
 import pytest
 import pylint
-import networkx
+import networkx as nx
 
 def args_check(argv):
     """This function collect parameters from the input command line, and check that every needed
@@ -50,7 +50,7 @@ def args_check(argv):
 def read_fastq(fastq_file):
     with open(fastq_file, "r") as file_in:
         for line in file_in:
-            yield next(file_in)
+            yield next(file_in).strip()
             next(file_in)
             next(file_in)
             
@@ -62,19 +62,82 @@ def build_kmer_dict(fastq_file, k):
     read_list = read_fastq(fastq_file) 
     kmer_dict = {}
     for i in read_list:
-        print(i)
-        for kmer in cut_kmer(i[:-1], k):
-            print(kmer)
+        for kmer in cut_kmer(i, k):
             if kmer in kmer_dict:
                 kmer_dict[kmer] = kmer_dict[kmer] + 1
             else:
                 kmer_dict[kmer] = 1
     return kmer_dict
 
+def build_graph(kmer_dict):
+    tree_graph = nx.DiGraph()
+    for i in kmer_dict:
+        tree_graph.add_edge(i[:-1], i[1:], weight=kmer_dict[i])
+    return tree_graph
+ 
+def get_starting_nodes(tree_graph):
+    starting_nodes = []
+    for node in tree_graph.nodes:
+        if len(tree_graph.pred[node]) == 0:
+            starting_nodes.append(node)
+    return starting_nodes
+
+def get_sink_nodes(tree_graph):
+    starting_nodes = []
+    for node in tree_graph.nodes:
+        if len(tree_graph.pred[node]) == 0:
+            starting_nodes.append(node)
+    return starting_nodes
+        
 def main():
     parameters = args_check(sys.argv[1:])
     kmer_dict = build_kmer_dict(parameters[0], parameters[1]) 
-    print(kmer_dict)
+    tree_graph = build_graph(kmer_dict)
+    starting_nodes = get_starting_nodes(tree_graph)
+    ending_nodes = get_sink_nodes(tree_graph)
+    print("START\n", starting_nodes)
+    print("END\n", ending_nodes)
+    return 0
+
+def std():
+    pass
+
+
+
+def path_average_weight():
+    pass
+
+
+def remove_paths():
+    pass
+
+
+def select_best_path():
+    pass
+
+
+def save_contigs():
+    pass
+
+
+def get_contigs():
+    pass
+
+
+def solve_bubble():
+    pass
+
+
+def simplify_bubbles():
+    pass
+
+
+def solve_entry_tips():
+    pass
+
+
+def solve_out_tips():
+    pass
 
 if __name__ == "__main__":
     main()
